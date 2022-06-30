4e2e399f0f1c914259db41d655c5ec15189ed83f
abdulqadir-jugnu <93977823+abdulqadir-jugnu@users.noreply.github.com>
2022-06-30 13:44:25 +0500

    beforeFirebaseSetup

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const App = () => {
+  // const onReady = () => {
+  //   RNBootSplash.hide({fade: true});
+  // };
+
+  return (
+    <View>
+      <Text>First Page</Text>
+    </View>
+  );
+};
+
+export default App;
